39795a1de1dca36920a4b21677ba2bcb59d763da
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2022-08-24 16:52:35 +0700

    Connect frontend with auth server for Google authentication and with user server for Discord code retrieval

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/connect" element={<Connect/>} />
       <Route path="/code" element={<Code/>} />
     </Routes>
   );
